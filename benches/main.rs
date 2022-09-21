de7cb0a581a78c566ad30d4f948053aa5fdd6778
David DiMaria <info@collectivesessions.com>
2022-09-21 08:18:46 -0600

    Initial benchmark setup for send/receive binding request/response

--- benches/main.rs
+++ benches/main.rs
@@ -1,16 +1,58 @@
+// use criterion::async_executor::async_tokio;
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use stun_server::client;
+use std::{net::SocketAddr, sync::Arc};
 
-fn fibonacci(n: u64) -> u64 {
-    match n {
-        0 => 1,
-        1 => 1,
-        n => fibonacci(n - 1) + fibonacci(n - 2),
-    }
+use bytes::BytesMut;
+use tokio::{net::UdpSocket, sync::Mutex};
+
+use stun_server::{config::CONFIG, error::Error, message::message::Message, server::server};
+
+static client_addr: SocketAddr = (*CONFIG).client.parse().unwrap();
+static server_addr: SocketAddr = (*CONFIG).server.parse().unwrap();
+
+async fn setup_server() {
+    dotenv::dotenv().ok();
+    pretty_env_logger::init();
+
+    // start the server in a separate thread
+    tokio::spawn(async { server().await });
 }
 
-fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("fib 20", |b| b.iter(|| fibonacci(black_box(20))));
+async fn setup_client() -> (UdpSocket, BytesMut) {
+    let socket = UdpSocket::bind(client_addr)
+        .await
+        .map_err(|e| Error::Startup(e.to_string()))
+        .unwrap();
+
+    let message = Message::binding_request(vec![]);
+
+    // encode the binding request
+    let mut bytes_mut = BytesMut::new();
+    message.encode(&mut bytes_mut);
+
+    (socket, bytes_mut)
+}
+
+// send the encoded binding request to the server
+async fn send_binding_request(socket: Arc<Mutex<UdpSocket>>, bytes: BytesMut) {
+    socket
+        .send_to(bytes.as_ref(), server_addr)
+        .await
+        .map_err(|e| Error::BindingRequest(e.to_string()))
+        .unwrap();
+}
+
+async fn criterion_benchmark(c: &mut Criterion) {
+    setup_server().await;
+    let (socket, bytes) = setup_client().await;
+    let rt = tokio::runtime::Runtime::new().unwrap();
+    let socket = Arc::new(Mutex::new(socket));
+
+    c.bench_function("send/receive binding request/response", |b| {
+        b.to_async(&rt).iter(|| async move {
+            send_binding_request(black_box(socket.clone()), black_box(bytes));
+        })
+    });
 }
 
 criterion_group!(benches, criterion_benchmark);
